9c6d05ff3f51dca022ec8f4c07a6f2c054d30fd2
Álvaro Mondéjar <mondejar1994@gmail.com>
2022-01-01 16:54:07 +0100

    Updated to new mdpo version (#23)

--- mkdocs_mdpo_plugin/mdpo_utils.py
+++ mkdocs_mdpo_plugin/mdpo_utils.py
@@ -2,18 +2,18 @@
 
 import re
 
-from mdpo.command import COMMAND_SEARCH_RE
+from mdpo.command import COMMAND_SEARCH_REGEX
 
 
-COMMAND_SEARCH_RE_AT_LINE_START = re.compile(
-    r'^(\s{2,})?[^\\]' + COMMAND_SEARCH_RE.pattern + r'\n?',
+COMMAND_SEARCH_REGEX_AT_LINE_START = re.compile(
+    r'^(\s{2,})?[^\\]' + COMMAND_SEARCH_REGEX + r'\n?',
     re.M,
 )
-COMMAND_SEARCH_RE_ESCAPER = re.compile(
+COMMAND_SEARCH_REGEX_ESCAPER = re.compile(
     (
-        r'\\(' + COMMAND_SEARCH_RE.pattern[:20] + ')('
-        + COMMAND_SEARCH_RE.pattern[20:38] + '?='
-        + COMMAND_SEARCH_RE.pattern[38:] + ')'
+        r'\\(' + COMMAND_SEARCH_REGEX[:20] + ')('
+        + COMMAND_SEARCH_REGEX[20:38] + '?='
+        + COMMAND_SEARCH_REGEX[38:] + ')'
     ),
     re.M,
 )
@@ -26,11 +26,11 @@ def remove_mdpo_commands_preserving_escaped(text):
         '<!-- mdpo',
         re.sub(
             # remove commands
-            COMMAND_SEARCH_RE_AT_LINE_START,
+            COMMAND_SEARCH_REGEX_AT_LINE_START,
             '',
             # preserve escaped commands
             re.sub(
-                COMMAND_SEARCH_RE_ESCAPER,
+                COMMAND_SEARCH_REGEX_ESCAPER,
                 r'\g<1>0-\g<2>',
                 text,
             ),
